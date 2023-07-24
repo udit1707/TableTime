ce127e75aba0d30fab7f594666a1627123cdb90a
Udit Singh <uditsingh294@gmail.com>
2023-07-24 12:06:48 +0530

    feat: Header UI Component

---
+++ components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import { MdKeyboardArrowDown } from "react-icons/md";
+import styles from "./Header.module.css";
+
+const Header = ({ location = "Madiwala New Extension, 1st Stage" }) => {
+  const address = location.split(" ");
+  const words = address.slice(0, 3).join(" ");
+  let locationText = words;
+
+  if (address.length > 3) {
+    locationText = words.slice(0,15) + "...";
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.label}>Pickup Now</div>
+      <div className={styles.locationContainer}>
+        <div className={styles.location}>{locationText}</div>
+        <MdKeyboardArrowDown className={styles.icon} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
