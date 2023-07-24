7fc019448d37810f0eed3079fec24ad59f1fac4c
Udit Singh <uditsingh294@gmail.com>
2023-07-24 13:14:41 +0530

    feat: Searchbar UI Component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Header from '@/components/Header/Header';
 import styles from '../styles/Index.module.css';
+import SearchBar from '@/components/SearchBar/SearchBar';
 
 const Index = () => {
   return ( <div className={styles.container}>
       <Header />
+      <SearchBar/>
   </div> );
 }
  
